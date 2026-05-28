8d3d1ec3104c2636564dc0c8e708e467aa64c82d
peter <peter.berghuis_13@hotmail.com>
2026-05-28 22:42:08 +0900

    update cloud run module: configure container commands, args, and tile server env vars; replace static image reference; remove unused outputs

--- variables.tf
+++ variables.tf
@@ -120,9 +120,3 @@ variable "app_storage_bucket_name" {
   description = "name of the app storage bucket name"
   type        = string
 }
-
-variable "tile_server_image" {
-  description = "The image for the tile server"
-  type        = string
-  default     = "maptiler/tileserver-gl:latest"
-}
