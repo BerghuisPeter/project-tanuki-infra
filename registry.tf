5b2cdaf2830123025250256e19358809fc14424e
peter <peter.berghuis_13@hotmail.com>
2026-06-11 13:55:34 +0900

    added cleanup policies

--- registry.tf
+++ registry.tf
@@ -3,6 +3,25 @@ resource "google_artifact_registry_repository" "tanuki_back_repo" {
   repository_id = var.gar_repository
   description   = "Docker repository for Tanuki backend services (Auth, Profile, etc.)"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest"
+    action = "KEEP"
+
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["latest"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-old"
+    action = "DELETE"
+
+    condition {
+      older_than = "604800s" # 7 days
+    }
+  }
 }
 
 resource "google_artifact_registry_repository" "angular_repo" {
@@ -10,6 +29,25 @@ resource "google_artifact_registry_repository" "angular_repo" {
   repository_id = var.angular_gar_repo
   description   = "Docker repository for Angular frontend"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest"
+    action = "KEEP"
+
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["latest"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-old"
+    action = "DELETE"
+
+    condition {
+      older_than = "604800s"
+    }
+  }
 }
 
 resource "google_artifact_registry_repository" "socket_repo" {
@@ -17,4 +55,23 @@ resource "google_artifact_registry_repository" "socket_repo" {
   repository_id = var.socket_gar_repo
   description   = "Docker repository for Socket server"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "keep-latest"
+    action = "KEEP"
+
+    condition {
+      tag_state    = "TAGGED"
+      tag_prefixes = ["latest"]
+    }
+  }
+
+  cleanup_policies {
+    id     = "delete-old"
+    action = "DELETE"
+
+    condition {
+      older_than = "604800s"
+    }
+  }
 }
